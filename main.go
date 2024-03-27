1156f96fcca9acb62daa59e2aee85826c47809fc
mariuszsienkiewicz <mariusz.sienkiewicz.it@gmail.com>
2024-03-27 17:40:00 +0100

    feat: add context to main func and exit on failure to run the app

--- main.go
+++ main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"context"
 	"os"
 	"proxy/cmd"
 	"proxy/modules/log"
 )
 
 func main() {
+	// setup context
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer func() {
+		cancel()
+	}()
+
+	// setup logger
 	log.SetLogger()
 
+	// setup app
 	log.Logger.Trace("Creating the app")
 	app := cmd.NewProxyApp()
 	log.Logger.Trace("Running the app")
-	_ = cmd.RunProxyApp(app, os.Args...)
+
+	// run the app, exit on failure
+	if err := cmd.RunProxyApp(app, os.Args...); err != nil {
+		log.Logger.Errorf("%s\n", err)
+		os.Exit(1)
+	}
 }
